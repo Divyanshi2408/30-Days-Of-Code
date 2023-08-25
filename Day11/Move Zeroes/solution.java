3ae29a1db7f9beeae1ee3f226519e9008dc0f84d
Divyanshi pal <85859444+Divyanshi2408@users.noreply.github.com>
2023-08-25 23:12:14 +0530

    Create solution.java

---
+++ Day11/Move Zeroes/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 1){
+            return;
+        }
+        int left = 0;
+        for(int right = 0;right<nums.length;right++){
+            if(nums[right]!=0){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        for(int i= left;i<nums.length;i++){
+            nums[i]=0;
+        }
+        
+    }
+}
